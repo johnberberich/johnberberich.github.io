9b44affcaeec6c8c28ac5e48257ebe42075ff687
John Berberich <jberberich@gmail.com>
2013-03-03 21:29:15 -0500

    Styling main nav and splitting out Google Analytics into separate include file.

--- staticize.py
+++ staticize.py
@@ -2,14 +2,25 @@
 import codecs
 from jinja2 import Environment, FileSystemLoader, Template
 
-loader = FileSystemLoader('templates')
-env = Environment(loader=loader)
 
-template_names = loader.list_templates()
-for template_name in template_names:
-	if not template_name.startswith('_'):
+def staticize():
+	loader = FileSystemLoader('templates')
+	env = Environment(loader=loader)
+	template_names = [t for t in loader.list_templates() if not t.startswith('_')]
+	for template_name in template_names:
+		print 'Staticizing ' + template_name
 		template = env.get_template(template_name)
 		html = template.render()
-		html_file = codecs.open(template_name, 'w', 'utf-8')
-		html_file.write(html)
-		html_file.close()
+		try:
+			html_file = codecs.open(template_name, 'w', 'utf-8')
+			try:
+				html_file.write(html)
+			finally:
+				html_file.close()
+		except IOError as e:
+			print 'IOError{}: Could not open '.format(e.errno) + template_name + ' for writing'
+			print e.strerror
+
+
+if __name__ == '__main__':
+	staticize()
