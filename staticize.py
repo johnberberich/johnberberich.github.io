e233e365b3f202d7b40cc0dc50ca1c935c1c699a
John Berberich <jberberich@gmail.com>
2013-05-22 06:03:44 -0400

    Adding Werkzeug and starting to implement dynamic renderer. May downgrade from Werkzeug to WSGI in the future, but I wanted to get it installed with pip while I had an Internet connection.

--- staticize.py
+++ staticize.py
@@ -24,8 +24,31 @@ def render(template_path):
             print e.strerror
 
 
+class StaticSite(object):
+    def __init__(self):
+        pass
+
+    def dispatch_request(self, request):
+        pass
+
+    def wsgi_app(self, environ, start_response):
+        request = Request(environ)
+        response = self.dispatch_request(request)
+        return response(environ, start_response)
+
+    def __call__(self, environ, start_response):
+        return self.wsgi_app(environ, start_response)
+
+
+def create_app():
+    app = StaticSite()
+    return app
+
+
 def run():
-    print 'This feature is not currently available.'
+    from werkzeug.serving import run_simple
+    app = create_app()
+    run_simple('127.0.0.1', 5000, app, use_debugger=True, use_reloader=True)
 
 
 if __name__ == '__main__':
